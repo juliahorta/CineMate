d67aa785c6d9f63f9b00a5b9d0fb7f4d07b54045
Julia <julia.magalhaes@btinternet.com>
2021-03-18 16:51:21 +0000

    Adding Diary Entries
    hard coded. no api usage yet.

--- app/models.py
+++ app/models.py
@@ -5,6 +5,22 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    entries = db.relationship('Diary', backref='logger', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)  
+
+class Diary(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date_watched = db.Column(db.DateTime)
+    movie_name = db.Column(db.String(200))
+    release_date = db.Column(db.DateTime)
+    user_rating = db.Column(db.Float())
+    rewatch = db.Column(db.Boolean(create_constraint=False))
+    review = db.Column(db.String(140))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '{} -> Movie: {}, Released: {}, Rating: {}, Rewatch?: {} '.format(self.date_watched,self.movie_name), self.release_date, self.user_rating, self.rewatch)
+
+        
