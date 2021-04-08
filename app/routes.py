3503e18401fc60849e0c06e34dc1799a02ddfb52
Julia <julia.magalhaes@btinternet.com>
2021-04-08 16:10:06 +0100

    hover styling

--- app/routes.py
+++ app/routes.py
@@ -29,8 +29,14 @@ def default():
 @app.route('/home')
 @login_required
 def home():
-    posts = current_user.followed_entries().all()
-    return render_template('index.html', title='Home', posts=posts)
+    page = request.args.get('page', 1, type=int)
+    posts = current_user.followed_entries().paginate(
+        page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('home', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('home', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('index.html', title='Home', posts=posts.items, next_url=next_url, prev_url=prev_url)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -71,8 +77,16 @@ def register():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
+    page = request.args.get('page', 1, type=int)
+    posts = user.entries.order_by(Diary.date_watched.desc()).paginate(
+        page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('user', username=user.username, page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('user', username=user.username, page=posts.prev_num) \
+        if posts.has_prev else None
     form = EmptyForm()
-    return render_template('user.html', user=user, form=form)
+    return render_template('user.html', user=user, posts=posts.items,
+                           next_url=next_url, prev_url=prev_url, form=form)
 
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
